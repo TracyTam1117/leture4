d216a5db5888d378814479a9a3fac181303493a2
Tracy Tam <135080253+TracyTam1117@users.noreply.github.com>
2026-03-18 04:14:11 +0000

    fix todo.py with correct task names

--- .github/scripts/todo.py
+++ .github/scripts/todo.py
@@ -20,38 +20,40 @@ class TaskPool:
         self.tasks = []
 
     def populate(self):
-        t1 = Task("Task 1")
-        t2 = Task("Task 2")
-        t3 = Task("Task 3")
-        t4 = Task("Task 4")
-        t5 = Task("Task 5")
-        t6 = Task("Task 6")
-        t1.mark_completed()
-        t2.mark_completed()
-        t3.mark_completed()
-        self.tasks = [t1, t2, t3, t4, t5, t6]
+        task1 = Task("Add Login UI", status="Done")
+        task2 = Task("Fix UI Bug", status="Done")
+        task3 = Task("Write Tests", status="Done")
+        task4 = Task("Update Login UI", status="ToDo")
+        task5 = Task("Update Documentation", status="ToDo")
+        task6 = Task("Deploy to Production", status="ToDo")
+        task1.mark_completed()
+        task2.mark_completed()
+        task3.mark_completed()
+        self.tasks = [task1, task2, task3, task4, task5, task6]
 
     def add_task(self, task):
         self.tasks.append(task)
 
     def get_open_tasks(self):
-        return [t for t in self.tasks if t.status == "ToDo"]
+        return [task for task in self.tasks if task.status == "ToDo"]
 
     def get_done_tasks(self):
-        return [t for t in self.tasks if t.status == "Done"]
+        return [task for task in self.tasks if task.status == "Done"]
 
 
 def main():
     pool = TaskPool()
     pool.populate()
-    todo = [t.title for t in pool.get_open_tasks()]
+
+    task_titles = [task.title for task in pool.get_open_tasks()]
     print("ToDo Tasks:")
-    for t in todo:
-        print(t)
-    done = [t.title for t in pool.get_done_tasks()]
-    print("Done Tasks:")
-    for t in done:
-        print(t)
+    for title in task_titles:
+        print(title)
+
+    completed_titles = [task.title for task in pool.get_done_tasks()]
+    print("\nDone Tasks:")
+    for title in completed_titles:
+        print(title)
 
 if __name__ == "__main__":
     main()
